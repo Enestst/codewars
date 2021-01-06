fb7d76d25cf55d002aae144c9d74f703cd82ff4e
Enestst <73773593+Enestst@users.noreply.github.com>
2021-01-06 14:27:18 +0300

    Create ConverttoBinary.py

---
+++ python/kyu8/ConverttoBinary.py
@@ -0,0 +1,9 @@
+def to_binary(n):
+    return int(bin(n).replace("0b", ""))
+    
+    
+"""
+def to_binary(n):
+    return int(f'{n:b}')  # we use string/format method, and :b formatting type for binary format.
+
+"""
