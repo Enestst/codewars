f8edffa7439a035109b1c56304a958516ade1a08
Enestst <73773593+Enestst@users.noreply.github.com>
2021-01-10 02:43:44 +0300

    Update Number_to_digit_tiers.py

--- python/kyu7/Number_to_digit_tiers.py
+++ python/kyu7/Number_to_digit_tiers.py
@@ -1,8 +1,8 @@
 def create_array_of_tiers(n):
-	list = []
+	lst = []
 	for i in range(1,len(str(n)) + 1):
-		list.append(str(n)[0:i])
-	return list
+		lst.append(str(n)[0:i])
+	return lst
 
 
 print(create_array_of_tiers(0))
